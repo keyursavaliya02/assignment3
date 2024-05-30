4b0907395659e294db02cb16c73542131d03654c
keyursavaliya02 <116817178+keyursavaliya02@users.noreply.github.com>
2024-05-30 19:22:51 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,1 @@
-package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
-
-// FahrenheitToCelsius converts a temperature from Fahrenheit to Celsius.
-func FahrenheitToCelsius(fahrenheit float64) float64 {
-	return (fahrenheit - 32) * 5 / 9
-}
-
-// CelsiusToFahrenheit converts a temperature from Celsius to Fahrenheit.
-func CelsiusToFahrenheit(celsius float64) float64 {
-	return (celsius * 9 / 5) + 32
-}
