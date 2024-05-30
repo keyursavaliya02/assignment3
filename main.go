8592c6d9f5e5dae5b7e7058043d1eb30a563a4f5
Pruthvi1881 <pruthvigadhiya28@gmail.com>
2024-05-30 19:27:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,7 @@
+func FahrenheitToCelsius(fahrenheit float64) float64 {
+	return (fahrenheit - 32) * 5 / 9
+}
 
+func CelsiusToFahrenheit(celsius float64) float64 {
+	return (celsius * 9 / 5) + 32
+}
